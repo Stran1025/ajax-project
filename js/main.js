d4719ddc544e992fc8ccfb29305d340e81e194bc
Sang Tran <sang.tran1025@gmail.com>
2022-04-19 17:06:53 +0000

    Getting data from api

--- js/main.js
+++ js/main.js
@@ -8,7 +8,27 @@ function handleClick(event) {
   $searchFilter.classList.toggle('hidden');
 }
 
-function createDiv(pokemonName) {
+var xhrGen1 = new XMLHttpRequest();
+xhrGen1.open('GET', 'https://pokeapi.co/api/v2/generation/1');
+xhrGen1.responseType = 'json';
+// xhrGen1.addEventListener('load', getlink(xhrGen1.response.pokemon_species));
+xhrGen1.send();
+
+// var xhrPokemon = new XMLHttpRequest();
+// xhrPokemon.open('GET', 'https://pokeapi.co/api/v2/pokemon/dragonite');
+// xhrPokemon.responseType = 'json';
+// xhrPokemon.send();
+
+function getlink(array) {
+  if (array[0].url === undefined) {
+    return;
+  }
+  for (var i = 0; i < array.length; i++) {
+    createDiv(array[i].url);
+  }
+}
+
+function createDiv(pokemonLink) {
 
   //  <div class="col-third display-top-space">
   //     <div class="pokemon-head center-width flex">
@@ -19,6 +39,10 @@ function createDiv(pokemonName) {
   //       <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png">
   //     </div>
   //   </div >
+  var xhrPokemon = new XMLHttpRequest();
+  xhrPokemon.open('GET', pokemonLink);
+  xhrPokemon.send();
+  var response = xhrPokemon.response;
 
   var $col = document.createElement('div');
   var $head = document.createElement('div');
@@ -32,17 +56,18 @@ function createDiv(pokemonName) {
   $body.className = 'pokemon-body center-width';
   $icon.className = 'width-fourth';
   $icon.setAttribute('src', 'images/Poké_Ball_icon.svg.png');
-  $image.setAttribute('src', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png');
+  $image.setAttribute('src', response.sprites.front_default);
   $h3.className = 'font-10';
-  $h3.textContent = pokemonName;
-
+  $h3.textContent = response.name;
   $col.appendChild($head);
   $col.appendChild($body);
   $head.appendChild($icon);
   $head.appendChild($h3);
   $body.appendChild($image);
 
+  $pokemonDisplay.appendChild($col);
+
   return $col;
 }
 
-$pokemonDisplay.appendChild(createDiv('test'));
+getlink();
