0466e1084e0c36e0bc340bb5771368771d35f529
Sang Tran <sang.tran1025@gmail.com>
2022-04-20 06:21:27 +0000

    A whole lotta work

--- js/data.js
+++ js/data.js
@@ -1,1 +1,7 @@
 /* exported data */
+
+var data = {
+  editing: null,
+  currentView: 'search',
+  team: []
+};
--- js/main.js
+++ js/main.js
@@ -4,15 +4,50 @@ var $pokemonDisplay = document.querySelector('#pokemon-display');
 var $searchBarArrow = document.querySelector('#search-bar-arrow');
 var $searchButton = document.querySelector('#search-button');
 var $searchForm = document.querySelector('#search-form');
+var $views = document.querySelectorAll('.view');
+var $customizePName = document.querySelector('#customize-pname');
+var $customizePImage = document.querySelector('#customize-pimage');
+var $customizeAbility = document.querySelector('#customize-ability');
 
 $searchBar.addEventListener('click', dropDownSearch);
 $searchButton.addEventListener('click', handleSearch);
+$pokemonDisplay.addEventListener('click', handleDisplayClick);
 
 var xhrGen1 = new XMLHttpRequest();
 xhrGen1.open('GET', 'https://pokeapi.co/api/v2/generation/1');
 xhrGen1.responseType = 'json';
 xhrGen1.addEventListener('load', handleXHR);
 xhrGen1.send();
+
+function handleDisplayClick(event) {
+  var clickedelement = event.target.parentElement.parentElement;
+  if (!clickedelement.hasAttribute('data-pokemon')) {
+    return;
+  }
+  var xhrEdit = new XMLHttpRequest();
+  xhrEdit.open('GET', 'https://pokeapi.co/api/v2/pokemon/' + clickedelement.getAttribute('data-pokemon'));
+  xhrEdit.responseType = 'json';
+  xhrEdit.addEventListener('load', loadclickedPokemon);
+  xhrEdit.send();
+  data.view = 'customize';
+  switchView(data.view);
+}
+
+function loadclickedPokemon(event) {
+  var response = event.target.response;
+  $customizePName.textContent = response.name[0].toUpperCase() + response.name.slice(1);
+  $customizePImage.setAttribute('src', response.sprites.front_default);
+  for (var abilityIndex = 0; abilityIndex < response.abilities.length; abilityIndex++) {
+    if (response.abilities[abilityIndex].is_hidden) {
+      continue;
+    } else {
+      var $abilityOption = document.createElement('option');
+      $abilityOption.textContent = response.abilities[abilityIndex].ability.name[0].toUpperCase() + response.abilities[abilityIndex].ability.name.slice(1);
+      $abilityOption.setAttribute('value', response.abilities[abilityIndex].ability.name);
+      $customizeAbility.appendChild($abilityOption);
+    }
+  }
+}
 
 function dropDownSearch(event) {
   $searchFilter.classList.toggle('hidden');
@@ -53,6 +88,20 @@ function handleXHR(event) {
   getlinkGeneration(returnData.pokemon_species);
 }
 
+function loadCurrentPokemon(event) {
+  $pokemonDisplay.appendChild(createDiv(event.target.response));
+}
+
+function switchView(view) {
+  for (var i = 0; i < $views.length; i++) {
+    if ($views[i].getAttribute('data-view') === view) {
+      $views[i].classList.remove('hidden');
+    } else {
+      $views[i].classList.add('hidden');
+    }
+  }
+}
+
 function getlinkGeneration(array) {
   for (var i = 0; i < array.length; i++) {
     sendRequest('https://pokeapi.co/api/v2/pokemon/' + array[i].name);
@@ -71,10 +120,6 @@ function sendRequest(url) {
   xhrPokemon.responseType = 'json';
   xhrPokemon.addEventListener('load', loadCurrentPokemon);
   xhrPokemon.send();
-}
-
-function loadCurrentPokemon(event) {
-  $pokemonDisplay.appendChild(createDiv(event.target.response));
 }
 
 function createDiv(obj) {
@@ -108,6 +153,7 @@ function createDiv(obj) {
   $image.setAttribute('src', obj.sprites.front_default);
   $h3.className = 'font-10';
   $h3.textContent = obj.name[0].toUpperCase() + obj.name.slice(1);
+  $col.setAttribute('data-pokemon', obj.name);
   $col.appendChild($head);
   $col.appendChild($body);
   $head.appendChild($icon);
