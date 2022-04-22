7ccfa175545eea929be5d954d92943fd2518b282
Sang Tran <sang.tran1025@gmail.com>
2022-04-22 18:17:58 +0000

    added local storage

--- js/data.js
+++ js/data.js
@@ -7,3 +7,15 @@ var data = {
   currentPokemon: null,
   team: []
 };
+
+window.addEventListener('beforeunload', saveData);
+
+function saveData(event) {
+  var dataJSON = JSON.stringify(data);
+  localStorage.setItem('pokemon-team-data', dataJSON);
+}
+
+var previousdataJSON = localStorage.getItem('pokemon-team-data');
+if (previousdataJSON !== null) {
+  data = JSON.parse(previousdataJSON);
+}
